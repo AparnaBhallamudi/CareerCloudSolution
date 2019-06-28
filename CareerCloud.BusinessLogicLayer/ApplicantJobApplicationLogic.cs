da6a66d24dd82775513afcca8bd4742658fd9e4f
Aparna Bhallamudi <bhallamudi.aparna@outlook.com>
2019-06-28 02:26:17 -0400

    updated Business Logic Layer

---
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -0,0 +1,115 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantJobApplicationLogic : BaseLogic<ApplicantJobApplicationPoco>
+    {
+        private const int saltLengthLimit = 10;
+
+        public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository)
+        {
+        }
+
+        
+        public override void Add(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantJobApplicationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantJobApplicationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            
+            foreach (var poco in pocos)
+            {
+                if ((poco.ApplicationDate) > DateTime.Today)
+                {
+                    exceptions.Add(new ValidationException(110, $"Application Date for {poco.Id} cannot be greater than today"));
+                }
+                
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+
+        private static byte[] GetSalt()
+        {
+            return GetSalt(saltLengthLimit);
+        }
+
+        private static byte[] GetSalt(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+            return salt;
+        }
+
+        private string ComputeHash(string plainText, byte[] saltBytes)
+        {
+            if (saltBytes == null)
+            {
+                saltBytes = GetSalt();
+            }
+
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] plainTextWithSaltBytes = new byte[plainTextBytes.Length + saltBytes.Length];
+            for (int i = 0; i < plainTextBytes.Length; i++)
+                plainTextWithSaltBytes[i] = plainTextBytes[i];
+
+            for (int i = 0; i < saltBytes.Length; i++)
+            {
+                plainTextWithSaltBytes[plainTextBytes.Length + i] = saltBytes[i];
+            }
+
+            HashAlgorithm hash = new SHA512Managed();
+            byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+            byte[] hashWithSaltBytes = new byte[hashBytes.Length + saltBytes.Length];
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hashWithSaltBytes[i] = hashBytes[i];
+            }
+            for (int i = 0; i < saltBytes.Length; i++)
+            {
+                hashWithSaltBytes[hashBytes.Length + i] = saltBytes[i];
+            }
+
+
+            return Convert.ToBase64String(hashWithSaltBytes);
+        }
+        private bool VerifyHash(string plainText, string hashValue)
+        {
+            const int hashSizeInBytes = 64;
+
+            byte[] hashWithSaltBytes = Convert.FromBase64String(hashValue);
+            if (hashWithSaltBytes.Length < hashSizeInBytes)
+                return false;
+            byte[] saltBytes = new byte[hashWithSaltBytes.Length - hashSizeInBytes];
+            for (int i = 0; i < saltBytes.Length; i++)
+                saltBytes[i] = hashWithSaltBytes[hashSizeInBytes + i];
+            string expectedHashString = ComputeHash(plainText, saltBytes);
+            return (hashValue == expectedHashString);
+        }
+
+    }
+}
